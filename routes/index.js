56ef96a46898b22413f7d93c74b61981bdcd0e22
Luis Daniel <luiyicpu@hotmail.com>
2018-10-09 22:22:27 -0400

    Added request promise to project Added fake data for listing Code refactoring

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var rp = require('request-promise');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+	var options = {
+		method: 'POST',
+		uri: '127.0.0.1:8081/api/v1/get_entidades',
+		headers: {
+            'Content-Type': 'application/json',
+        }
+	};
+	/*
+	rp(options).then((res) => {
+		res.render('index', { title: 'Control IPOS', entities: JSON.parse(res).response });
+	}).catch(function (err) {
+			
+	});
+	*/
+	//Fake data
+	res.render('index',
+	{ 
+		title: 'Control IPOS',
+		entities: {"0": {"idtipoEntidad": 1, "nomtipoEntidad": "Entidad I"}, "1": {"idtipoEntidad": 2, "nomtipoEntidad": "Entidad II"}}
+	});
 });
 
 module.exports = router;
