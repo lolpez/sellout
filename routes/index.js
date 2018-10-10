996d635d9deb2ef8dcc363a0d4f449fd94bb62c0
Luis Daniel <luiyicpu@hotmail.com>
2018-10-09 23:13:26 -0400

    Added simple circles

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,14 @@ router.get('/', (req, res, next) => {
 	//Fake data
 	res.render('index',	{ 
 		title: 'Control IPOS',
-		entities: {"0": {"idtipoEntidad": 1, "nomtipoEntidad": "Entidad I"}, "1": {"idtipoEntidad": 2, "nomtipoEntidad": "Entidad II"}}
+		entities: {
+			"0": {"idtipoEntidad": 1, "nomtipoEntidad": "Entidad I"},
+			"1": {"idtipoEntidad": 2, "nomtipoEntidad": "Entidad II"},
+			"2": {"idtipoEntidad": 3, "nomtipoEntidad": "Entidad III"},
+			"3": {"idtipoEntidad": 4, "nomtipoEntidad": "Entidad IV"},
+			"4": {"idtipoEntidad": 5, "nomtipoEntidad": "Entidad V"},
+			"5": {"idtipoEntidad": 6, "nomtipoEntidad": "Entidad VI"},
+		}
 	});
 });
 
