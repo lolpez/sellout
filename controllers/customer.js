5986dd12308f57bb75197632081bbaf7caeb2259
Luis Daniel LDLS. Lopez Sandi <Daniel.Lopez@dbi.local>
2018-10-15 17:09:19 -0400

    Refactored ws calls

--- controllers/customer.js
+++ controllers/customer.js
@@ -1,5 +1,11 @@
+var path = require('path');
+var nconf = require('nconf');
+nconf.argv().env().file({file: path.join(__dirname, '..', 'config.json')});
+
 module.exports = {
-	addCustomer: (req, res, next) => {
+	wsName: module.filename.slice(__filename.lastIndexOf(path.sep)+1, module.filename.length -3),
+	addCustomer: (req, res, next) => {		
+		console.log(`Call to WS ${nconf.get(url)}/${nconf.get(apiUrl)}/${nconf.get(apiVersion)}/${nconf.get(wsName).add}`)
 		console.log(req.body.data);
 		res.send({message: "exito", object: req.body.data})
 	}
