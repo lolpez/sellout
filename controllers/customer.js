291ff67a51e0419baea06bb14cc4f6f149677f29
Luis Daniel <luiyicpu@hotmail.com>
2018-10-15 23:51:54 -0400

    Fixed customer POST route

--- controllers/customer.js
+++ controllers/customer.js
@@ -1,11 +1,27 @@
+var rp = require('request-promise');
 var path = require('path');
 var nconf = require('nconf');
 nconf.argv().env().file({file: path.join(__dirname, '..', 'config.json')});
 
 module.exports = {
-	wsName: module.filename.slice(__filename.lastIndexOf(path.sep)+1, module.filename.length -3),
-	addCustomer: (req, res, next) => {		
-		console.log(`Call to WS ${nconf.get(url)}/${nconf.get(apiUrl)}/${nconf.get(apiVersion)}/${nconf.get(wsName).add}`)
+	addCustomer: (req, res, next) => {
+		//Call Python API for customer insert
+		/*var options = {
+			method: 'POST',
+			uri: `${nconf.get("url")}/${nconf.get("apiUrl")}/${nconf.get("apiVersion")}/${nconf.get("customer").add}`,
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: {
+				data: req.body.data
+			},
+		};	
+		rp(options).then((object) => {
+			res.send({message: "exito", object: object})
+		}).catch(function (err) {
+		});*/		
+
+		//Fake response
 		console.log(req.body.data);
 		res.send({message: "exito", object: req.body.data})
 	}
