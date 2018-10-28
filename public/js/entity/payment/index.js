15f45a8ca8733cba47bb4a950a54ca39c94471a3
lolpez <lopezsandi1666@gmail.com>
2018-10-28 19:48:44 -0400

    Added logic for payment total

--- public/js/entity/payment/index.js
+++ public/js/entity/payment/index.js
@@ -21,6 +21,7 @@ Cart.prototype.addProduct = function(product) {
     var quantityCell = row.insertCell(2);
     var priceCell = row.insertCell(3);
     var totalCell = row.insertCell(4);
+    totalCell.id = `product-total-${id}`;
     nameCell.innerHTML = this.products[id].name;
     typeCell.innerHTML = "TIPO?";
     //quantityCell.innerHTML = this.products[id].quantity;
@@ -43,6 +44,7 @@ Cart.prototype.addProduct = function(product) {
     quantityCell.appendChild(buttonContainer);
     priceCell.innerHTML = this.products[id].price;
     totalCell.innerHTML = this.products[id].price;
+    this.updatePaymentTotal();
 }
 
 Cart.prototype.createButtonControl = function(buttonType, productName, id) {
@@ -66,7 +68,6 @@ Cart.prototype.createButtonControl = function(buttonType, productName, id) {
     return button;
 }
 
-
 Cart.prototype.doAction = function(action, id) {
     this[action](id);
 }
@@ -82,6 +83,7 @@ Cart.prototype.add = function(id) {
     this.products[id].quantity ++;
     var quantityText = document.getElementById(`quantity-${id}`);
     quantityText.innerHTML = this.products[id].quantity;
+    this.updateProductTotal(id);
 }
 
 Cart.prototype.remove = function(id) {
@@ -91,6 +93,7 @@ Cart.prototype.remove = function(id) {
         this.products[id].quantity --;
         var quantityText = document.getElementById(`quantity-${id}`);
         quantityText.innerHTML = this.products[id].quantity;
+        this.updateProductTotal(id);
     }
 }
 
@@ -98,4 +101,20 @@ Cart.prototype.delete = function(id) {
     var row = document.getElementById(`row-${id}`);
     row.parentNode.removeChild(row);
     delete this.products[id];
+    this.updatePaymentTotal();
+}
+
+Cart.prototype.updateProductTotal = function(id) {
+    var productTotal = document.getElementById(`product-total-${id}`);
+    productTotal.innerHTML = this.products[id].price * this.products[id].quantity;
+    this.updatePaymentTotal();
+}
+
+Cart.prototype.updatePaymentTotal = function() {
+    var paymentTotal = document.getElementById(`payment-total`);
+    var total = 0;
+    for (var id in this.products) {
+        total += this.products[id].price * this.products[id].quantity;
+    }
+    paymentTotal.innerHTML = total;
 }
